50cbb9ac384f3620a1ddc47c32aefc931b6e7e7d
Dannansilva <dannangunathilaka@gmail.com>
2023-08-29 14:09:32 +0530

    text and images

--- blooming/lib/page/home_page.dart
+++ blooming/lib/page/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class home extends StatelessWidget {
@@ -17,7 +19,7 @@ class home extends StatelessWidget {
           ],
         ),
       ),
-      child: const Scaffold(
+      child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -39,10 +41,70 @@ class home extends StatelessWidget {
                 ),
               ],
             ),
-            Text('BLOOMING'),
-            Text('CUPCAKES'),
-            Text('& BOUQUETS'),
-            
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'BLOOMING',
+                    style: TextStyle(fontSize: 35),
+                  ),
+                  Text(
+                    'CUPCAKES',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    '& BOUQUETS',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/cupcakes.jpg",
+                      width: 180,
+                      height: 100,
+                    ),
+                    Text("cupcakes"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/minicake.jpg",
+                      width: 100,
+                      height: 100,
+                    ),
+                    Text('minicake'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Image.asset("assets/boquets.jpg",width: 180 ,height: 100 ,),
+                    Text("boquets"),
+                  ],
+                ),
+              ],
+            ),
+            // Image.asset("assets/cupcakes.jpg",width: 180, height: 100 ,),
+            // Image.asset("assets/minicake.jpg",width: 100,height: 180,),
+            // Image.asset("assets/boquets.jpg",width: 180 ,height: 180,),
+            // Image.asset("assets/giftbox.jpg",width: 180,height: 180,),
           ],
         ),
       ),
